b8989258ac61cc8376dde920311c6b5db87f48d7
samuel <samuel.bagattin@gmail.com>
2020-10-15 15:06:35 +0200

    Add entity

--- src/test/java/com/cesi/bankonet/BankonetApplicationTests.java
+++ src/test/java/com/cesi/bankonet/BankonetApplicationTests.java
@@ -2,12 +2,14 @@ package com.cesi.bankonet;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.Assertions;
 
 @SpringBootTest
 class BankonetApplicationTests {
 
     @Test
     void contextLoads() {
+        Assertions.assertTrue(true);
     }
 
 }
