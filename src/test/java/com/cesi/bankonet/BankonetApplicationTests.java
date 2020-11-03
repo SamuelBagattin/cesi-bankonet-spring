531126030bc0f3565f2ccde5684dcd62dd639b53
samuel <samuel.bagattin@gmail;com>
2020-11-03 08:43:01 +0100

    Fix

--- src/test/java/com/cesi/bankonet/BankonetApplicationTests.java
+++ src/test/java/com/cesi/bankonet/BankonetApplicationTests.java
@@ -1,6 +1,5 @@
 package com.cesi.bankonet;
 
-import com.cesi.bankonet.models.Account;
 import com.cesi.bankonet.models.CurrentAccount;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
